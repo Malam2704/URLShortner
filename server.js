8ba847e2fa9e3ccb78642619a9e1f2878fe8e022
Moe Alam <ma3655@rit.edu>
2023-07-13 09:19:07 -0400

    this is a deprecated project

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const expres = require("express")
-const app = expres()
+const express = require("express")
+const mongoose = require("mongoose")
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortner', {
+    useNewURLParser: true, useUnifiedTopology: true
+})
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-    res.render('index')
+app.get("/", async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+})
+
+app.post("/shortUrls", async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5000);
